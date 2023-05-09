a5a38d208a28116eedc1e4d5354bc4e9356aa3ba
Bravo1B9 <brofe93@gmail.com>
2023-05-09 12:51:47 +0000

    Implement add task functionality

--- linked-list.ts
+++ linked-list.ts
@@ -5,5 +5,18 @@ class Todo {
   constructor(task: string, nextTodo: any = null){
     this.task = task;
     this.nextTodo = nextTodo;
-  }
-}
+  };
+};
+
+class TodoList {
+  head: any;
+  constructor() {
+    this.head = null;
+  };
+
+  addTask(task: string) {
+    const newTodo = new Todo(task);
+    this.head = newTodo
+  };
+
+};
