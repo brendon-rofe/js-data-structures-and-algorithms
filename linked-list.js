59f6f5250baa5f846e2dfa1cb865584fa0f62376
Bravo1B9 <brofe93@gmail.com>
2023-05-10 15:34:38 +0000

    Initial setup

--- linked-list.js
+++ linked-list.js
@@ -1,19 +1,9 @@
-var Priority;
-(function (Priority) {
-    Priority[Priority["Low"] = 0] = "Low";
-    Priority[Priority["Medium"] = 1] = "Medium";
-    Priority[Priority["High"] = 2] = "High";
-})(Priority || (Priority = {}));
-;
-;
 var Todo = /** @class */ (function () {
-    function Todo(title, description, priority) {
-        this.data = {
-            title: title,
-            description: description,
-            completed: false,
-            priority: priority
-        };
+    function Todo(title, description) {
+        this.next = null;
+        this.title = title,
+            this.description = description,
+            this.completed = false;
     }
     ;
     return Todo;
@@ -24,8 +14,8 @@ var TodoList = /** @class */ (function () {
         this.head = null;
     }
     ;
-    TodoList.prototype.addTodo = function (title, description, priority) {
-        var newTodo = new Todo(title, description, priority);
+    TodoList.prototype.addTodo = function (title, description) {
+        var newTodo = new Todo(title, description);
         if (!this.head) {
             this.head = newTodo;
         }
@@ -34,14 +24,15 @@ var TodoList = /** @class */ (function () {
             while (current.next) {
                 current = current.next;
             }
+            ;
             current.next = newTodo;
         }
         ;
     };
     ;
     TodoList.prototype.getAllTodos = function () {
-        var current = this.head;
         var todos = [];
+        var current = this.head;
         while (current) {
             todos.push(current);
             current = current.next;
@@ -50,26 +41,10 @@ var TodoList = /** @class */ (function () {
         console.log(todos);
     };
     ;
-    TodoList.prototype.getTodoByIndex = function (index) {
-        var current = this.head;
-        var count = 0;
-        while (current) {
-            if (count === index) {
-                console.log(current.data);
-            }
-            ;
-            count++;
-            current = current.next;
-        }
-        ;
-        return null;
-    };
-    ;
     return TodoList;
 }());
 ;
 var todoList = new TodoList();
-todoList.addTodo('First todo', 'This is the first todo', Priority.Medium);
-todoList.addTodo('Second todo', 'This is the second todo', Priority.Low);
-// todoList.getAllTodos();
-todoList.getTodoByIndex(1);
+todoList.addTodo('Go to gas and more', 'Get new heater and check lamp');
+todoList.addTodo('Learn linked lists', 'Practice creating linked lists');
+todoList.getAllTodos();
