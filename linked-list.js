849148b850245856486fb224a37a54d198d0ce41
Bravo1B9 <brofe93@gmail.com>
2023-05-12 10:00:31 +0000

    Create ITodo and Todo

--- linked-list.js
+++ linked-list.js
@@ -1,34 +1,12 @@
+;
 var Todo = /** @class */ (function () {
     function Todo(title, description) {
-        this.title = title;
-        this.description = description;
-        this.completed = false;
         this.next = null;
+        this.data.title = title;
+        this.data.description = description;
+        this.data.completed = false;
     }
     ;
     return Todo;
 }());
 ;
-var TodoList = /** @class */ (function () {
-    function TodoList() {
-        this.head = null;
-    }
-    ;
-    TodoList.prototype.addTodo = function (title, description) {
-        var newTodo = new Todo(title, description);
-        var currentTodo = this.head;
-        if (!currentTodo) {
-            this.head = newTodo;
-        }
-        else {
-            currentTodo.next = newTodo;
-        }
-        ;
-    };
-    ;
-    return TodoList;
-}());
-;
-var todoList = new TodoList();
-todoList.addTodo('Go to the gym', 'Lift weights');
-console.log(todoList);
